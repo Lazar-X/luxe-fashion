d0c08a1472cd31ebef96088e8889e20350ccbf8b
Lazar-X <lazarjankovicbusiness@gmail.com>
2023-04-26 13:35:37 +0200

    Make single-product page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 window.onload = function() {
+    quantity();
+    toggleMenu();
+}
+
+// Function for navigation
+function toggleMenu() {
     const toggle = document.querySelector('#toggle-button');
     const navbar = document.querySelector('#menu');
     console.log(navbar);
@@ -6,5 +12,32 @@ window.onload = function() {
     toggle.addEventListener('click', () => {
         navbar.classList.toggle('active');
         toggle.classList.toggle('active');
+    });
+}
+
+// Function for quantity on single-product page
+function quantity() {
+    const buttonPlus = document.querySelector('#button-operator-plus');
+    const buttonMinus = document.querySelector('#button-operator-minus');
+    const buttonResult = document.querySelector('#button-operator-result');
+    const quantityMessage = document.querySelector('#quantity-help');
+
+    buttonPlus.addEventListener('click', () => {
+        if(buttonResult.value >= 1 && buttonResult.value < 10) {
+            buttonResult.value++;
+            quantityMessage.textContent = "";
+        }
+        if(buttonResult.value >= 10) {
+            quantityMessage.textContent = "Quantity cannot exceed 10. If you need to order more, please contact the administrator.";
+        }
+    });
+    buttonMinus.addEventListener('click', () => {
+        if(buttonResult.value <= 1) {
+            quantityMessage.textContent = "Quantity cannot be negative.";
+        }
+        if(buttonResult.value >= 2) {
+            buttonResult.value--;
+            quantityMessage.textContent = "";
+        }
     });
 }
