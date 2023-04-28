38dd623ce51c438943ce72e500e55aac1285a7a9
Lazar-X <lazarjankovicbusiness@gmail.com>
2023-04-28 12:36:19 +0200

    Fix some thing, try to validate contact data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 window.onload = function() {
     // Global
     toggleMenu();
+    // Index
+    if(document.URL.includes('index.html')) {
+
+    }
     // Shop
     if(document.URL.includes('shop.html')) {
         const sizes = document.querySelector("#div-sizes").querySelectorAll('label');
         addActiveClass(sizes, 'active-size');
+    }
+    // About us
+    if(document.URL.includes('about-us.html')) {
+        
+    }
+    // Contact
+    if(document.URL.includes('contact.html')) {
+        contactValidation();
     }
     // Single product
     if(document.URL.includes('single-product.html')) {
@@ -42,7 +54,6 @@ function quantity() {
     const buttonResult = document.querySelectorAll('.button-operator-result');
     const quantityMessage = document.querySelectorAll('.quantity-help');
 
-
     for (let i = 0; i < buttonPlus.length; i++) {
         buttonPlus[i].addEventListener('click', () => {
             if(buttonResult[i].value >= 1 && buttonResult[i].value < 10) {
@@ -64,25 +75,6 @@ function quantity() {
             }
         });
     }
-
-    // buttonPlus.addEventListener('click', () => {
-    //     if(buttonResult.value >= 1 && buttonResult.value < 10) {
-    //         buttonResult.value++;
-    //         quantityMessage.textContent = "";
-    //     }
-    //     if(buttonResult.value >= 10) {
-    //         quantityMessage.textContent = "Quantity cannot exceed 10. If you need to order more, please contact the administrator.";
-    //     }
-    // });
-    // buttonMinus.addEventListener('click', () => {
-    //     if(buttonResult.value <= 1) {
-    //         quantityMessage.textContent = "Quantity cannot be negative.";
-    //     }
-    //     if(buttonResult.value >= 2) {
-    //         buttonResult.value--;
-    //         quantityMessage.textContent = "";
-    //     }
-    // });
 }
 
 // Function for adding active classes
@@ -110,4 +102,64 @@ function showPassword() {
               }
         });
     }
+}
+
+// Function to validate contact data
+function contactValidation() {
+    $(document).on('click', '#contactButton', function() {
+        let name = $('#nameContact');
+        let email = $('#emailContact');
+        let message = $('#messageContact');
+
+        let regexName = /^[A-Z][a-z]{2,19}( [A-Z][a-z]{2,19})*$/;
+        let regexEmail = /^([a-zA-Z0-9._%+-]{1,64})@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/;
+        let regexMessage = /^[A-Z][\w\s\d.,!?-]{9,49}$/i;
+
+        let errorCounter = 0;
+
+        if(!regexName.test($(name).value())) {
+            errorCounter++;
+            $('#nameHelp').addClass('text-danger');
+        }
+        else {
+            $('#nameHelp').removeClass('text-danger');
+        }
+
+        if(!regexEmail.test($(email).value())) {
+            errorCounter++;
+            $('#emailHelp').addClass('text-danger');
+        }
+        else {
+            $('#emailHelp').removeClass('text-danger');
+        }
+
+        if(!regexMessage.test($(message).value())) {
+            errorCounter++;
+            $('#message').addClass('text-danger');
+        }
+        else {
+            $('#message').removeClass('text-danger');
+        }
+
+        // if(errorCounter == 0) {
+        //     let contactData = [
+        //         'name': $(name).val(),
+        //         'email': $(email).val(),
+        //         'message': $(message).val()
+        //     ];
+
+        //     $.ajax({
+        //         url: 'contact_handler.php',
+        //         method: 'post',
+        //         dataType: 'json',
+        //         success: function(result) {
+        //             console.log(result);
+        //         },
+        //         error: function(xhr) {
+        //             console.log(xhr);
+        //         }
+        //     });
+        // }
+
+    });
 }
