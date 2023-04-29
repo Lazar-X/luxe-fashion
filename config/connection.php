1e06fa20255d53e3799b76f37eb7fb0c89186454
Lazar-X <lazarjankovicbusiness@gmail.com>
2023-04-29 19:52:19 +0200

    Make connection file

---
+++ config/connection.php
@@ -0,0 +1,14 @@
+<?php
+    define("SERVER", "localhost");
+    define("DATABASE", "luxefashion");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    try {
+    $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE.";charset=utf8", USERNAME, PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
+    catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
