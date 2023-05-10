cb59a0ae47e793f9c65bf3bdd79985fe6b8c555b
Lazar-X <lazarjankovicbusiness@gmail.com>
2023-05-10 15:19:41 +0200

    Repare links

--- pages/about-us.php
+++ pages/about-us.php
@@ -24,12 +24,12 @@
             </div>
             <div class="row mb-lg-5 mb-2">
                 <div class="offset-md-3 col-md-9 col-12 shadow">
-                    <p class="text-md-right text-left">Over time, we expanded our selection of products, added new brands and collections, and built a strong online presence. We are proud of what we have accomplished and grateful to our customers for their support. Check our <a href="shop.html" class="about-us-link">shop</a>.</p>
+                    <p class="text-md-right text-left">Over time, we expanded our selection of products, added new brands and collections, and built a strong online presence. We are proud of what we have accomplished and grateful to our customers for their support. Check our <a href="shop.php" class="about-us-link">shop</a>.</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-8 col-12 shadow">
-                    <p class="text-left">We love hearing from our customers! If you have any questions, comments, or concerns, please don't hesitate to reach out to us. Our friendly and knowledgeable customer service team is always ready to help you with anything you need. You can <a href="contact.html" class="about-us-link">contact us</a> through our website or social media channels, and we'll get back to you as soon as possible.</p>
+                    <p class="text-left">We love hearing from our customers! If you have any questions, comments, or concerns, please don't hesitate to reach out to us. Our friendly and knowledgeable customer service team is always ready to help you with anything you need. You can <a href="contact.php" class="about-us-link">contact us</a> through our website or social media channels, and we'll get back to you as soon as possible.</p>
                 </div>
             </div>
         </div>
